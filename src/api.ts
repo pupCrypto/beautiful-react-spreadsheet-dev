9c2f5180dc8f2567df8594b6d8efc65fb6576e18
pupCrypto <ujif_cryptoriders@mail.ru>
2024-10-06 12:55:48 +0800

    add skeleton of apis

--- src/api.ts
+++ src/api.ts
@@ -1,1 +1,21 @@
-class GlobalApi {}
+export function useSpreadsheetApi() {
+  return {
+    setWidth: (colIdx: number, rowIdx: number, width: number) => {
+      console.log(colIdx, rowIdx, width);
+    },
+    setHeight: (colIdx: number, rowIdx: number, height: number) => {
+      console.log(colIdx, rowIdx, height);
+    },
+  };
+}
+
+export function useCellApi(colIdx: number, rowIdx: number) {
+  return {
+    set width(w: number) {
+      console.log(colIdx, rowIdx, w);
+    },
+    set height(h: number) {
+      console.log(colIdx, rowIdx, h);
+    },
+  };
+}
